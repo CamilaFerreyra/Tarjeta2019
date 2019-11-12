553dbd2df81882815c72eeb058c27c226edbf322
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2019-11-12 19:34:39 -0300

    Arreglado test de colectivo, agregado test de tiempo y eliminados tests de tarjeta

--- tests/TiempoTest.php
+++ tests/TiempoTest.php
@@ -5,6 +5,16 @@ namespace TrabajoTarjeta;
 use PHPUnit\Framework\TestCase;
 
 class TiempoTest extends TestCase {
-    
-    
+
+    public function testTiempo() {
+        // Test de tiempo falso
+        $tiempo_falso = new Tiempo(1);
+
+        $this->assertEquals(1, $tiempo_falso->tiempo());
+
+        // Test de tiempo verdadero
+        $tiempo = new Tiempo();
+
+        $this->assertEquals(time(), $tiempo->tiempo());
+    }
 }
